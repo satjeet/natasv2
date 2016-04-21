aecb63c961c4017208684bc0e5cc4b6b21b88270
satjeet <arturo.alfaro87@gmail.com>
2016-04-21 06:06:20 -0300

    arreglado el warning del login django1.10

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -14,11 +15,10 @@ urlpatterns = [
 
     # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     # login y logout por defecto en django, esperan estar en templates/registration
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/$', auth_views.login, name='login'),
     #url(r'^logouto/$', 'django.contrib.auth.views.logout',{'template_name': 'registration/logged_out.html'}, name='logout'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/pedidos/'}, name='logout'),
-    url(r'^logout_then_login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/pedidos/'},name='logout'),
+    url(r'^logout_then_login/$', auth_views.logout_then_login, name='logout_then_login'),
 
 ]
 
-url
